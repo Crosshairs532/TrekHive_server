e2c3f9fe941d55982f5defaf8e35faec96282005
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-01-08 01:27:43 +0600

    token implemented

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 4000;
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 app.use(cors());
 app.use(express.json())
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 console.log(process.env.DB_USER);
 const uri = 'mongodb+srv://trekhive:n5QZQGRJ8jd0UkRa@cluster0.wtx9jbs.mongodb.net/TrekHive?retryWrites=true&w=majority';
 // DB_USER=trekhive
@@ -29,10 +33,30 @@ const Dbconnect = async () => {
     }
 }
 Dbconnect();
-
 const AllusersCollection = client.db('TrekHive').collection('users');
 const PackagesCollection = client.db('TrekHive').collection('packages');
 const BookingsCollection = client.db('TrekHive').collection('Bookings');
+
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.Authorization;
+    if (!token) {
+
+        return res.status(401).send({ message: 'Unauthorized' })
+    }
+    // verifying the token
+    jwt.verify(token, process.env.TOKEN_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
+        req.decode = decoded;
+        next();
+    })
+}
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('TrekHive Server is running')
@@ -48,36 +72,46 @@ app.get('/packages', async (req, res) => {
     console.log(result);
     res.send(result);
 })
-app.get('/booking', async (req, res) => {
-    const result = await BookingsCollection.find().toArray();
+app.get('/booking', verifyToken, async (req, res) => {
+    const { email } = req.query;
+    const userEmail = email;
+    const filter = {};
+    if (userEmail) {
+        filter.TouristEmail = userEmail;
+    }
+    const result = await BookingsCollection.find(filter).toArray();
+    res.send(result);
+})
+app.get('/users', async (req, res) => {
+
+    const result = await AllusersCollection.find().toArray();
     res.send(result);
 })
 
 
+
+app.post('/jwt', (req, res) => {
+    const userEmail = req.body;
+    const token = jwt.sign(userEmail, process.env.TOKEN_KEY, { expiresIn: '2h' });
+    res.send({ token: token })
+})
 app.post('/users', async (req, res) => {
     const user = req.body;
+    console.log(user);
     const exist = await AllusersCollection.findOne({ email: user?.email });
     if (exist) {
-        res.send({ message: 'already exist' })
+        return res.send({ message: 'already exist' })
     }
-    else {
-        const result = await AllusersCollection.insertOne(user);
-        res.send(result)
-    }
+
+    const result = await AllusersCollection.insertOne(user);
+    res.send(result)
+
 })
-
 app.post('/booking', async (req, res) => {
-
     const booking = req.body;
     const result = await BookingsCollection.insertOne(booking);
     res.send(result);
-
 })
-
-
-
-
-
 
 
 
